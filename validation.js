26fcd251e2af154cc52b5688c6a90a622f5cc5aa
Adikwu Samuel <Adikwu.S@bullseye.local>
2023-03-23 09:14:15 +0100

    Card_validation

---
+++ validation.js
@@ -0,0 +1,23 @@
+
+
+function ccValidation(){
+  const a = document.getElementById("cardno"); 
+  const btn = document.getElementById("btn");
+let input = a.value;
+  var cardno = /^(?:5[1-5][0-9]{14})$/;
+  if (input.match(cardno))
+  {
+    a.style="border-color:green";
+    btn.style="background-color:green";
+    return true;
+  }
+      else
+  {
+    
+    a.style="border-color:red";
+    btn.style="background-color:red";
+    alert("Not a valid credit card number!");
+    return false;
+  }
+}
+
